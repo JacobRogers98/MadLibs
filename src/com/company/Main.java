d10c252059d63522b77accb302a67c809d6122ba
EighthTax <brazilbound09@yahoo.com>
2019-12-14 21:34:14 -0600

    Added inputs

---
+++ src/com/company/Main.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String adj1;
+        String pluralNoun1;
+        String pluralNoun2;
+        String color;
+        String number1;
+        String verb;
+        String typeOfLiquid;
+        String adj2;
+        String pluralNoun3;
+        String number2;
+        String adj3;
+        String noun1;
+        String sillyWord;
+        String place;
+        String adj4;
+        String noun2;
+        String verbEndingING;
+        String partOfBodyPlural;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an adjective.");
+        adj1 = scanner.nextLine();
+        System.out.println("Enter a plural noun.");
+        pluralNoun1 = scanner.nextLine();
+        System.out.println("Enter another plural noun.");
+        pluralNoun2 = scanner.nextLine();
+        System.out.println("Enter a color.");
+        color = scanner.nextLine();
+        System.out.println("Enter a number.");
+        number1 = scanner.nextLine();
+        System.out.println("Enter a verb.");
+        verb = scanner.nextLine();
+        System.out.println("Enter a type of liquid.");
+        typeOfLiquid = scanner.nextLine();
+        System.out.println("Enter an adjective.");
+        adj2 = scanner.nextLine();
+        System.out.println("Enter a plural noun.");
+        pluralNoun3 = scanner.nextLine();
+        System.out.println("Enter a number.");
+        number2 = scanner.nextLine();
+        System.out.println("Enter an adjective.");
+        adj3 = scanner.nextLine();
+        System.out.println("Enter a noun.");
+        noun1 = scanner.nextLine();
+        System.out.println("Enter a silly word.");
+        sillyWord = scanner.nextLine();
+        System.out.println("Enter a place.");
+        place = scanner.nextLine();
+        System.out.println("Enter an adjective.");
+        adj4 = scanner.nextLine();
+        System.out.println("Enter a noun.");
+        noun2 = scanner.nextLine();
+        System.out.println("Enter a verb ending in 'ing'.");
+        verbEndingING = scanner.nextLine();
+        System.out.println("Enter a part of the body (plural).");
+        partOfBodyPlural = scanner.nextLine();
+        System.out.println("\n\n\n");
+    }
+}
